ca0021a0f469017fedf44a4e24eb16a4ce0b2f42
pmmachadov <pmmachadov@gmail.com>
2024-02-01 19:06:31 +0100

    Code change to use only js

--- script.js
+++ script.js
@@ -2,25 +2,7 @@ let fact = document.querySelector('#fact');
 let factText = document.querySelector('#factText');
 
 let numberInput = document.querySelector('#numberInput');
-numberInput.addEventListener('input', getFactAjax);
-
-// Fetch with XHR. HHR is a built-in object in web browsers that allows to make HTTP requests in JavaScript
-function getFactAjax() {
-    let number = numberInput.value;
-    if (number != '') {
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', 'http://numbersapi.com/' + number);
-
-        xhr.onload = function () {
-            if (this.status == 200) {
-                fact.style.display = 'block';
-                factText.innerText = this.responseText;
-            }
-        }
-
-        xhr.send();
-    }
-}
+numberInput.addEventListener('input', getFactFetch);
 
 // Fetch with Fetch API
 function getFactFetch() {
@@ -36,3 +18,21 @@ function getFactFetch() {
             .catch(err => console.log(err));
     }
 }
+
+// Fetch with XHR. HHR is a built-in object in web browsers that allows to make HTTP requests in JavaScript
+// function getFactAjax() {
+//     let number = numberInput.value;
+//     if (number != '') {
+//         let xhr = new XMLHttpRequest();
+//         xhr.open('GET', 'http://numbersapi.com/' + number);
+
+//         xhr.onload = function () {
+//             if (this.status == 200) {
+//                 fact.style.display = 'block';
+//                 factText.innerText = this.responseText;
+//             }
+//         }
+
+//         xhr.send();
+//     }
+// }
